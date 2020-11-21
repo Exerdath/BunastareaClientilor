29e109e296894b26a080793c27fe5494cdd3285b
Exerdath <nicolescu.aurel@yahoo.com>
2020-11-21 15:23:29 +0200

    Updates at customers/ migrations

--- bunastarea-clientilor-backend/app.py
+++ bunastarea-clientilor-backend/app.py
@@ -2,21 +2,28 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from customerspy import DataAnalysis
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usersv2.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
-class User(db.Model):
-    __table_name__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(32), index = True)
+class Usersv2(db.Model):
+    __table_name__ = 'usersv2'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(128))
+    isAdmin = db.Column(db.Boolean)
+    customerId = db.Column(db.Integer)
 
     def __repr__(self):
-        return 'Username: ' + str(self.username) + " Password: " + str(self.password_hash)
+        return 'Username: ' + str(self.username) + " Password: " + str(self.password_hash) + " Admin: " + str(self.isAdmin)
 
 
 @app.route('/')
@@ -33,10 +40,10 @@ def add_user():
     if username is None or password is None:
         return jsonify({'msg': 'No user or password provided'}), 400
 
-    if db.session.query(User).fitler_by(username=username).first() is not None:
+    if db.session.query(Usersv2).filter_by(username=username).first() is not None:
         return jsonify({'msg': 'User already exists'}), 400
 
-    user = User(username=username, password_hash=password)
+    user = Usersv2(username=username, password_hash=password, isAdmin=False, customerId=1)
     db.session.add(user)
     db.session.commit()
     return jsonify({"user": username, "password": password}), 200
@@ -44,7 +51,7 @@ def add_user():
 
 @app.route('/getUsers', methods=['GET'])
 def get_users():
-    users = db.session.query(User).all()
+    users = db.session.query(Usersv2).all()
     all_users_json = [ob.__dict__ for ob in users]
     for user_json in all_users_json:
         user_json.pop("_sa_instance_state", None)
@@ -60,11 +67,16 @@ def login():
     if username is None or password is None:
         return jsonify({'msg': 'No user or password provided'}), 400
 
-    user = User(username=username, password_hash=password)
-    user_db = db.session.query(User).filter_by(username=user.username)\
+    user = Usersv2(username=username, password_hash=password)
+    user_db = db.session.query(Usersv2).filter_by(username=user.username)\
         .filter_by(password_hash=user.password_hash).first()
     if user_db is not None:
-        return jsonify({'msg': 'Got into Narnia, Good Job mate!'}), 200
+        return jsonify({
+            'username': user_db.username,
+            'password': user_db.password_hash,
+            'isAdmin': user_db.isAdmin,
+            'customerId': user_db.customerId
+        }), 200
     else:
         return jsonify({'msg': 'Failed!'}), 403
 
@@ -83,14 +95,14 @@ def avg_buyers5():
     return the_buyers
 
 
-@app.route("/avgbyid/<int:user_id>",methods=['GET'])
+@app.route("/avgbyid/<int:user_id>", methods=['GET'])
 def avg_spend_by_id(user_id):
     data_analyzer = DataAnalysis()
     the_buyers = data_analyzer.avg_spent_per_invoice_by_id(user_id)
     return the_buyers
 
 
-@app.route("/lastinvoicesbyid/<int:user_id>",methods=['GET'])
+@app.route("/lastinvoicesbyid/<int:user_id>", methods=['GET'])
 def last_invoices_by_id(user_id):
     data_analyzer = DataAnalysis()
     the_buyers = data_analyzer.last_invoices_by_id(user_id)
@@ -98,4 +110,4 @@ def last_invoices_by_id(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
